e163f24eb64b2748fae35f58f21309021d99470d
Evelette Didier <foledem@hotmail.com>
2021-10-05 13:25:37 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,52 @@
+# EOSP Threadpool
+
+## Description
+
+**EOSP ThreadPool** is a header-only templated thread pool writtent in c++17. It is designed to be easy to use while being able to execute any type of function. You only need two functions to use the thread pool. `addTask()`to add a function to execute and `waitForTask()` to wait the task is done.
+When threads are waiting for a result from other threads they compute new functions from the task pool until the result they need is available.
+
+## Exemple
+
+~~~~
+
+#include <future>
+#include <iostream>
+#include "eosp/Threadpool.hpp"
+
+
+using namespace std;
+
+int f1(int x, int y)
+{
+    return x*(y%x);
+}
+
+void f2()
+{
+    cout << "Hello World !" << endl;
+}
+
+int main()
+{
+    ThreadPool Tpool;				//create a default thread pool
+
+    std::future<void> fut2 = Tpool.addTask(f2);		//add f2 in the task queue	
+
+    std::future<int> fut1 = Tpool.addTask(f1, 7, 31);	//add f1 in the task queue
+    int result = Tpool.waitForTask(fut1);	//wait the result of f1 and store the return value
+
+    Tpool.waitForTask(fut2);			//wait the end of the execution of f2 (void function)
+
+    cout << result << endl;
+}
+~~~~
+
+* * *
+
+Since threads start to work on new tasks to wait before they return the first task, it can create some recursion. `ThreadPool` can be used with parameters to chose the number of thread and the maximum recursion depth.
+
+`ThreadPool(nbrThread, depth)` creates a thread pool with `nbrThread` thread (default value is the number of CPU threads) And `depth` the maximum recursion depth which defines the number of times an unfinished task can start a new one to wait (default value is 5).
+
+`addTask(fct, param)` adds a function `fct` with `param` as parameter to the pool to be compute. There can be no parameter, one or more. `addTask()` return an `std::future<T>` where T is the return type of the `fct`function.
+
+`WaitForTask(fut)` waits the task is done and return its return value if there is one. It has the same return type as `fct` or no return type if `fct` is a void function.
---
+++ Threadpool.hpp
@@ -0,0 +1,145 @@
+#ifndef THREADPOOL_HPP
+#define THREADPOOL_HPP
+
+#include <atomic>
+#include <functional>
+#include <future>
+#include <map>
+#include <mutex>
+#include <stack>
+#include <thread>
+#include <utility>
+#include <vector>
+
+class ThreadPool
+{
+public:
+    ThreadPool(unsigned int thread = std::thread::hardware_concurrency(), unsigned int maxRecursionDepth=5):
+        maxRecursion(maxRecursionDepth),
+        threadNumber(thread)
+    {
+        if(threadNumber==0)
+            threadNumber=std::thread::hardware_concurrency();
+
+        for(unsigned int i=0;i<threadNumber;i++)
+        {
+            RecursionMap.emplace(std::hash<std::thread::id>()(workers.emplace_back(std::thread([this]{workerLoop();})).get_id()), 0);//Create thread, fill map with hash of thread id and set recursion to 0
+        }
+        RecursionMap.emplace(std::hash<std::thread::id>()(std::this_thread::get_id()),0);//add hash of the main thread in the map
+    }
+    ~ThreadPool()
+    {
+        stop = true;
+        cv.notify_all();
+
+        for(unsigned int i=0;i<threadNumber;i++)
+        {
+            workers.at(i).join();
+        }
+
+    }
+
+    template <typename F, typename...Args>
+    auto addTask(F&& f, Args&&... args) -> std::future<decltype (f(args...))>
+    {
+        using ReturnType = decltype(f(args...));
+
+        if(stop)
+            return{};
+
+        if(RecursionMap.find(std::hash<std::thread::id>()(std::this_thread::get_id()))->second < maxRecursion)//if recursion isn't too deep
+        {
+
+            std::function<ReturnType()> func = std::bind(std::forward<F>(f), std::forward<Args>(args)...);
+            auto pTask = std::make_shared<std::packaged_task<ReturnType()>>(func);
+            std::function<void()> wrapperFunc = [pTask](){(*pTask)();};
+
+            auto result = pTask->get_future();
+            {
+                std::lock_guard lk(taskFrame.mtx);
+                taskFrame.taskQueue.emplace(wrapperFunc);//add task in queue
+                cv.notify_one();
+            }
+            return result;
+        }
+        else//if recursion is too deep do not add in the queue. Direct execute
+        {
+            std::promise<ReturnType> result;
+            if constexpr(std::is_same_v<ReturnType, void>)
+            {
+                f(args...);
+                result.set_value();
+            }
+            else
+            {
+                result.set_value(f(args...));
+            }
+            return result.get_future();
+        }
+    }
+
+    template <typename T>
+    auto waitForTask(std::future<T>& fut)
+    {
+        while(!stop)
+        {
+            std::unique_lock lock(taskFrame.mtx);
+            if(fut.wait_for(std::chrono::seconds(0))==std::future_status::ready)//if the task is done then return the result
+                return fut.get();
+
+            tryToWork(lock);//else take an other task
+        }
+        return static_cast<T>(NULL);//void or other
+    }
+
+private:
+
+    void tryToWork(std::unique_lock<std::mutex>& lock)
+    {
+
+        if(stop)
+            return;
+        if(taskFrame.taskQueue.size() == 0)//if there is no task in queue
+        {
+            cv.wait(lock);//wait other threads
+            return;
+        }
+
+        std::function<void()> task = std::move((taskFrame.taskQueue.top()));//else take one in the queue
+        taskFrame.taskQueue.pop();
+        lock.unlock();
+        RecursionMap.find(std::hash<std::thread::id>()(std::this_thread::get_id()))->second ++;//increment recursion
+        task();
+        RecursionMap.find(std::hash<std::thread::id>()(std::this_thread::get_id()))->second --;
+        std::lock_guard notifyLock(taskFrame.mtx);
+        cv.notify_all();
+
+    }
+
+    void workerLoop()
+    {
+        while(!stop)
+        {
+            std::unique_lock lock(taskFrame.mtx);
+            tryToWork(lock);//take a task in the queue if there are otherwise wait
+        };
+    }
+
+private:
+
+    std::atomic<bool> stop {false};
+    unsigned int maxRecursion;
+    unsigned int threadNumber;
+    std::condition_variable cv;
+
+    struct {
+    std::mutex mtx;
+    std::stack<std::function<void()>> taskQueue;
+    } taskFrame;
+
+    std::vector<std::thread> workers;
+    std::map<size_t, unsigned int> RecursionMap;
+
+};
+
+#endif // THREADPOOL_HPP
