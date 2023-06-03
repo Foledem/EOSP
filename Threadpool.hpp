5a94d57099f1c3a4976da7bd9f8dd7f89bba2c62
Evelette Didier <foledem@hotmail.com>
2023-06-03 23:05:33 +0200

    Performance improvement : Use thread id instead of its hash to store in the map Replace std::map by std::unordered_map Minor other improvements

--- Threadpool.hpp
+++ Threadpool.hpp
@@ -4,7 +4,7 @@
 #include <atomic>
 #include <functional>
 #include <future>
-#include <map>
+#include <unordered_map>
 #include <mutex>
 #include <queue>
 #include <thread>
@@ -23,9 +23,10 @@ public:
 
         for(unsigned int i=0;i<threadNumber;i++)
         {
-            RecursionMap.emplace(std::hash<std::thread::id>()(workers.emplace_back(std::thread([this]{workerLoop();})).get_id()), 0);//Create thread, fill map with hash of thread id and set recursion to 0
+            std::thread::id id = workers.emplace_back(std::thread([this]{workerLoop();})).get_id();//Create thread and get its id
+            RecursionMap.emplace(id, 0);//Fill map with thread id and set recursion to 0
         }
-        RecursionMap.emplace(std::hash<std::thread::id>()(std::this_thread::get_id()),0);//add hash of the main thread in the map
+        RecursionMap.emplace(std::this_thread::get_id(),0);//add main thread id in the map
     }
     ~ThreadPool()
     {
@@ -40,24 +41,23 @@ public:
     }
 
     template <typename F, typename...Args>
-    auto addTask(F&& f, Args&&... args) -> std::future<decltype (f(args...))>
+    std::future<typename std::result_of<F(Args...)>::type> addTask(F&& f, Args&&... args)
     {
-        using ReturnType = decltype(f(args...));
+        using ReturnType = typename std::result_of<F(Args...)>::type;
 
         if(stop)
             return{};
 
-        if(RecursionMap.find(std::hash<std::thread::id>()(std::this_thread::get_id()))->second < maxRecursion)//if recursion isn't too deep
+        if(RecursionMap[std::this_thread::get_id()] < maxRecursion)//if recursion isn't too deep
         {
+            std::function<ReturnType()> boundFunc = std::bind(std::forward<F>(f), std::forward<Args>(args)...);//Bind f function
+            std::shared_ptr<std::packaged_task<ReturnType()>> pFunc = std::make_shared<std::packaged_task<ReturnType()>>(boundFunc);//Keep a reference
+            std::function<void()> func = [pFunc](){(*pFunc)();};
 
-            std::function<ReturnType()> func = std::bind(std::forward<F>(f), std::forward<Args>(args)...);
-            auto pTask = std::make_shared<std::packaged_task<ReturnType()>>(func);
-            std::function<void()> wrapperFunc = [pTask](){(*pTask)();};
-
-            auto result = pTask->get_future();
+            std::future<ReturnType> result = pFunc->get_future();
             {
-                std::lock_guard lk(taskFrame.mtx);
-                taskFrame.taskQueue.emplace(wrapperFunc);//add task in queue
+                std::lock_guard<std::mutex> lock(taskFrame.mtx);
+                taskFrame.taskQueue.emplace(func);//add task in queue
                 cv.notify_one();
             }
             return result;
@@ -76,21 +76,22 @@ public:
             }
             return result.get_future();
         }
-    }
+}
 
     template <typename T>
-    auto waitForTask(std::future<T>& fut)
+    T waitForTask(std::future<T>& fut)
     {
         while(!stop)
         {
-            std::unique_lock lock(taskFrame.mtx);
-            if(fut.wait_for(std::chrono::seconds(0))==std::future_status::ready)//if the task is done then return the result
+            std::unique_lock<std::mutex> lock(taskFrame.mtx);
+            if(fut.wait_for(std::chrono::seconds(0)) == std::future_status::ready)//if the task is done then return the result
                 return fut.get();
 
             tryToWork(lock);//else take an other task
         }
         return static_cast<T>(NULL);//void or other
     }
+
 
 private:
 
@@ -99,28 +100,31 @@ private:
 
         if(stop)
             return;
-        if(taskFrame.taskQueue.size() == 0)//if there is no task in queue
+        if(taskFrame.taskQueue.empty())//if there is no task in queue
         {
             cv.wait(lock);//wait other threads
             return;
         }
+        else
+        {
+            std::function<void()> task = std::move((taskFrame.taskQueue.front()));//else take one in the queue
+            taskFrame.taskQueue.pop();
+            lock.unlock();
 
-        std::function<void()> task = std::move((taskFrame.taskQueue.front()));//else take one in the queue
-        taskFrame.taskQueue.pop();
-        lock.unlock();
-        RecursionMap.find(std::hash<std::thread::id>()(std::this_thread::get_id()))->second ++;//increment recursion
-        task();
-        RecursionMap.find(std::hash<std::thread::id>()(std::this_thread::get_id()))->second --;
-        std::lock_guard notifyLock(taskFrame.mtx);
-        cv.notify_all();
+            RecursionMap[std::this_thread::get_id()] ++;//increment recursion
+            task();
+            RecursionMap[std::this_thread::get_id()] --;
 
+            std::lock_guard<std::mutex> notifyLock(taskFrame.mtx);
+            cv.notify_all();
+        }
     }
 
     void workerLoop()
     {
         while(!stop)
         {
-            std::unique_lock lock(taskFrame.mtx);
+            std::unique_lock<std::mutex> lock(taskFrame.mtx);
             tryToWork(lock);//take a task in the queue if there are otherwise wait
         };
     }
@@ -138,7 +142,7 @@ private:
     } taskFrame;
 
     std::vector<std::thread> workers;
-    std::map<size_t, unsigned int> RecursionMap;
+    std::unordered_map<std::thread::id, unsigned int> RecursionMap;
 
 };
 
